f1409d942dd6ccff709377e140419c144818619f
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-09 10:42:10 +0530

    define postmonger

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,4 +1,6 @@
-var connection = new Postmonger.Session();
+define(["postmonger"], function (Postmonger) {
+    "use strict";
+    var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function (data)
@@ -10,4 +12,5 @@ connection.on('clickedNext', function()
 {
     var configuration = JSON.parse(document.getElementById('configuration').value);
     connection.trigger('updateActivity', configuration);
-});
+})
+})
