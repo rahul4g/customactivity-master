1132f9a24c0d9d6e14405b43f74c7960d55ef016
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-13 17:38:48 +0530

    jhgfsjdgfjdsf

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('', activity.save );
+app.post('', activity.validate );
+app.post('', activity.publish );
+app.post('', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
