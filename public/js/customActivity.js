b84d9a009a0587796fce88cb3ae33b2bb2609b39
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-13 12:15:52 +0530

    yhftryfgff

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -6,98 +6,17 @@ define([
     'use strict';
 
     var connection = new Postmonger.Session();
-    var payload = {};
-    var lastStepEnabled = false;
-    var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Step 1", "key": "step1" }
-    ];
-    var currentStep = steps[0].key;
+    connection.trigger('ready');
 
-    $(window).ready(onRender);
+    connection.on('initActivity', function (data)
+    {
+        document.getElementById('configuration').value = JSON.stringify(data,null,2);
+    });
 
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-
-    connection.on('clickedNext', save);
-  
-
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
-
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-
-        
-
-        
-    }
-
-    function initialize (data) {
-        console.log(data);
-        if (data) {
-            payload = data;
-        }
-
-        var message;
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        $.each(inArguments, function(index, inArgument) {
-            $.each(inArgument, function(key, val) {
-                if (key === 'message') {
-                    message = val;
-                }
-            });
-        });
-
-        connection.trigger('updateButton',{
-            button:'next',
-            text:'done',
-            visible:true
-        });
-
-        
-    }
-
-    function onGetTokens (tokens) {
-        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        // console.log(tokens);
-    }
-
-    function onGetEndpoints (endpoints) {
-        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        console.log(endpoints);
-    }
-
-    
-
-    
-
-    function save() {
-       
-        // 'payload' is initialized on 'initActivity' above.
-        // Journey Builder sends an initial payload with defaults
-        // set by this activity's config.json file.  Any property
-        // may be overridden as desired.
-        
-
-        payload['arguments'].execute.inArguments = [{"phone_number":"{{Contact.Attribute.whatsapp_activity.PhoneNumber}}"},
-        {"name":"{{Contact.Attribute.whatsapp_activity.Name}}"},
-        {"template_id":"{{Contact.Attribute.whatsapp_activity.TemplateId}}"}];
-
-        payload['metaData'].isConfigured = true;
-
-        connection.trigger('updateActivity', payload);
-    }
-
-    
-
+    connection.on('clickedNext', function()
+    {
+        var configuration = JSON.parse(document.getElementById('configuration').value);
+        connection.trigger('updateActivity', configuration);
+    })
 });
+
