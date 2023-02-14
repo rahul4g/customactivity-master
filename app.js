b8285ba2d1ab692b3754804d5478d387f1a8fbdb
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-14 17:53:55 +0530

    initial

--- app.js
+++ app.js
@@ -1,43 +1,73 @@
-'use strict';
-// Module Dependencies
-// -------------------
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var errorhandler = require('errorhandler');
-var http        = require('http');
-var path        = require('path');
-var request     = require('request');
-var routes      = require('./routes');
-var activity    = require('./routes/activity');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-var app = express();
+app.get("/", (req, res) => res.type('html').send(html));
 
-// Configure Express
-app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.raw({type: 'application/jwt'}));
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//app.use(express.methodOverride());
-//app.use(express.favicon());
 
-app.use(express.static(path.join(__dirname, 'public')));
+const html = `
+<!DOCTYPE html>
 
-// Express in Development Mode
-if ('development' == app.get('env')) {
-  app.use(errorhandler());
-}
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Custom Journey Builder Activity</title>
 
-// HubExchange Routes
-app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
+    <script type="application/json" src="js/jquery.min.js"></script>
+    <script type="application/json" src="js/require.js"></script>
+    <script type="application/json" src="js/customActivity.js"></script>
+    <script type="text/javascript">
+      (function () {
+        var config = {
+          baseUrl: "js",
+        };
 
-// Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+        var dependencies = ["customActivity"];
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+        require(config, dependencies);
+      })();
+    </script>
+
+    <!--Styles-->
+    <style type="text/css">
+      body {
+        padding: 20px;
+        margin: 0;
+      }
+      .step {
+        display: none;
+      }
+      #step1 {
+        display: block;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="step1" class="step">
+      Select your SMS message:
+      <select id="select1">
+        <option value="">Select an option</option>
+        <option value="someMessage">Some SMS</option>
+        <option value="anotherMessage">Another SMS</option>
+        <option value="thirdMessage">Third SMS</option>
+        <option value="unknownMessage">Unknown SMS</option>
+      </select>
+
+      <button id="toggleLastStep">Toggle Last Step</button>
+    </div>
+    <div id="step2" class="step">
+      Here's a second page with some info on it.
+    </div>
+    <div id="step3" class="step">
+      You chose the message:
+      <div id="message"></div>
+    </div>
+    <div id="step4" class="step">
+      Hey, here's a fourth step, just for you. Toggle this on or off from step
+      1.
+    </div>
+  </body>
+</html>
+`
