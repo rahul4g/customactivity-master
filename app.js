979ec2561f5a7764e663469a2bc2aaceba4845d0
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-15 11:56:39 +0530

    fdfd

--- app.js
+++ app.js
@@ -2,57 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', routes.index );
 
 
-const html = `
-<!DOCTYPE html>
 
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Custom Journey Builder Activity</title>
-
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/require.js"></script>
-    <script type="text/javascript" src="js/postmonger.js"></script>
-    <script type="text/javascript">
-        (function() {
-            var config = {
-                baseUrl: "js",
-            };
-
-            var dependencies = [
-                "customActivity"
-            ];
-
-            require(config, dependencies);
-        })();
-    </script>
-
-    <!--Styles-->
-    <style type="text/css">
-        body {
-            padding: 20px;
-            margin: 0;
-        }
-        .step {
-            display: none;
-        }
-        #step1 {
-            display: block;
-        }
-    </style>
-
-</head>
-<body>
-    <h3>Configuration JSON</h3>
-    
-<textarea id = "configuration"></textarea>
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 
-  </body>
-</html>
-`
