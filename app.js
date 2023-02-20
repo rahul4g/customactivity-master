468accc11b7933ebebbdb021286f924daed3f7f1
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-20 18:02:18 +0530

    tr

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3001;
 var routes = require('./routes');
+var activity    = require('./routes/activity');
 
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
@@ -18,5 +19,12 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
 
