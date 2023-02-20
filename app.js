97be988966b9e78386743b83508698b06e923d2f
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-20 16:50:49 +0530

    tr

--- app.js
+++ app.js
@@ -2,32 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-
-
-const html = `
-<!DOCTYPE html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Custom Journey Builder Activity</title>
-
-    <script type="application/json"  src="js/jquery.min.js"></script>
-    <script type="application/json"  src="js/require.js"></script>
-    <script type="application/json"  src = "js/postmonger.js"></script>
-    <script  type="application/json"  src = "js/activity.js"></script>
-
-    
-  </head>
-  <body>
-    <h3>Configuration JSON</h3>
-    
-<textarea id = "configuration"></textarea>
-
-
-  </body>
-</html>
-`;
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', routes.index );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
