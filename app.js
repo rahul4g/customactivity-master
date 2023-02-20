ebb7e844d3dbc3b260a917158ea826b6365f41cf
Raj <rwsrajnish@gmail.com>
2023-02-20 17:36:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 var routes = require('./routes');
 
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/public'));
+
 //app.get('/', routes.index );
 
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/public/index.html'));
+});*/
+
+
+app.get('/', function(req, res) {
+    res.render('index.html');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
